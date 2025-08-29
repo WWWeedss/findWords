4e409baea03e1ffedf044f2d3eb0d29ccbd78df9
Apprivoisé <221840039@smail.nju.edu.cn>
2025-08-30 00:16:22 +0800

    chore: 更改 primeVue 至版本 4

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
+import './assets/index.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
+import Aura from '@primeuix/themes/aura'
 import 'primeicons/primeicons.css'
 const app = createApp(App)
 
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }})
 app.mount('#app').$nextTick(() => {
   // Use contextBridge
   window.ipcRenderer.on('main-process-message', (_event, message) => {
